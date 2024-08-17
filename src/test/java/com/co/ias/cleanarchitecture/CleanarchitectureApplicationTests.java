0b6e9927a81746035b4e3f5524875ab3f7627a5e
Chave <rchave2801@hotmail.com>
2024-08-17 10:15:53 -0500

    Initial commit

---
+++ src/test/java/com/co/ias/cleanarchitecture/CleanarchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.ias.cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
